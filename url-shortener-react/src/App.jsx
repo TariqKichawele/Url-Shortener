eac2352c0f687eaab28d8643773918dfcb4fc48a
TariqKichawele <tariqkichawele66@gmail.com>
2025-03-01 12:09:50 -0500

    Add registration page with form validation using react-hook-form

--- url-shortener-react/src/App.jsx
+++ url-shortener-react/src/App.jsx
@@ -1,21 +1,23 @@
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import AboutPage from './components/AboutPage'
+import RegisterPage from './components/RegisterPage'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path='/about' element={<AboutPage />}/>
+          <Route path='/register' element={<RegisterPage />} />
         </Routes>
         <Footer />
-      </BrowserRouter>
+      </Router>
     </>
   )
 }
