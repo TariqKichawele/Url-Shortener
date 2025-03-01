4bfdfde1d3e8bd929210724daff6956312551426
TariqKichawele <tariqkichawele66@gmail.com>
2025-03-01 11:10:50 -0500

    Set up React frontend with Tailwind CSS, routing, and initial landing page components

--- url-shortener-react/src/App.jsx
+++ url-shortener-react/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
 
 function App() {
   return (
     <>
-      <div>
-        Hello
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
