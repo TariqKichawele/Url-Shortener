c01591cfdd8e2a54c47a86d28109afd9c1fb1cd3
TariqKichawele <tariqkichawele66@gmail.com>
2025-03-01 11:33:13 -0500

    Add Navbar and Footer components, update App routing

--- url-shortener-react/src/App.jsx
+++ url-shortener-react/src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import AboutPage from './components/AboutPage'
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
+          <Route path='/about' element={<AboutPage />}/>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   )
